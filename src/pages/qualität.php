1885bce979fdbcb768d039119dc8ab80580a9a88
lindx <linder.igor@gmail.com>
2021-05-10 00:08:00 +0300

    Qualität

--- src/pages/qualität.php
+++ src/pages/qualität.php
@@ -5,107 +5,101 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/static/css/main.css">
+  <link rel="stylesheet" href="/static/css/pages.css">
+  <link rel="stylesheet" href="/static/css/slider.css">
   <title>Main page</title>
-
-  <link rel="stylesheet" href="../static/css/bootstrap.css">
-  <link rel="stylesheet" href="../static/css/style.css">
-  <link rel="stylesheet" href="../static/css/slider.css">
-
 </head>
 
 <body>
+  <div class="wrapper">
 
-  <div class="container">
-
-    <div class="row"></div>
-
-    <div class="navigate">
+    <header class="header">
       <?php include "../nav/header.html" ?>
+    </header>
+
+    <div class="shadow-container-pages">
+      <div class="shadow-frame">
+        <div class="shadow"></div>
+      </div>
     </div>
 
+    <div class="preview-block-pages">
 
-    <div id="page-logo">
+      <div class="container">
 
-      <img src="../static/img/qu.png">
-
+        <div class="about-pages">
+          <p>Unternehmen</p>
+          <h1>Qualität</h1>
+        </div>
+      </div>
+      <img src="../static/img/qu.png" id="preview">
     </div>
 
+    <div class="content">
 
+      <div class="content-block-row white" style="padding-bottom: 171px;">
 
+        <div class="container">
 
-    <div class="about-static">
-      <span style="font-size: 135%;">Unternehmen</span>
-      <p>Qualität</p>
-    </div>
+          <div class="col-1">
 
+            <h1>
+              Zertifiziertes Qualitätsmanagement & maximale Verlässlichkeit
 
-
-    <div class="content-section row">
-      <div class="col" style="padding: 100px; margin-right: 15%;">
-
-        <h2 style="width: 85%; margin-bottom: 27px;">
-          <strong>
-            Zertifiziertes Qualitätsmanagement & maximale Verlässlichkeit
-          </strong>
-        </h2>
-
-        <div class="row">
-
-          <div class="col col-lg-12" style="padding-bottom: 2%;">
+            </h1>
 
             <p>
               Seit jeher steht die Qualität aus einer Vielzahl von Gründen bei uns im Mittelpunkt. Vor allem deshalb, weil eine hohe Qualität der elektronischen Bauteile und Komponenten auch mit einer großen Langlebigkeit und besseren Funktionalität einhergeht. Dies ist unseren Kunden besonders wichtig, weshalb wir dies auch für unseren Einkauf zur Priorität gemacht haben. So unterziehen jeglichen Bauelementen, die wir für unsere Kunden einkaufen, umfassenden Tests. Dazu setzen wir auf bewährte Mess- und Prüfverfahren, welche im Zuge unseres DIN EN ISO 9001 zertifizierten Qualitätsmanagements durchgeführt werden.
+
             </p>
+
           </div>
 
-          <div class="col-lg-1" style="text-align: right;">
-            <img src="../static/img/sh1.png" alt="">
-          </div>
+          <div class="col-2">
+            <div>
+              <img src="../static/img/sh1.png" alt="">
+            </div>
 
-          <div class="col col-lg-5">
             <p>
               Unsere Untersuchungen der elektronischen Bauteile umfasst neben der visuellen Inspektion auch eine Röntgeninspektion. Es folgt ein RoHS Test, eine genaue optische Fehleranalyse sowie die Bauteilöffnung. Neben einem Aceton-, einem Funktions- und einem Lötbarkeitstest führen wir auch einen elektrischen Test durch. Im Anschluss gibt es eine Digitalmikroskopie, eine BGA und eine Void Inspektion Void sowie ein Datenblatt-Vergleich. Durch dieses Vorgehen stellen wir bau jedem einzelnen elektrischen Bauteil die maximale Qualität für unsere Kunden sicher.
             </p>
 
-          </div>
+            <div>
+              <img src="../static/img/sh2.png" alt="">
+            </div>
 
-          <div class="col-lg-1" style="text-align: right;">
-            <img src="../static/img/sh2.png" alt="">
-          </div>
-
-          <div class="col col-lg-5">
             <p>
               Doch nicht nur in den produktiven, sondern auch den administrativen Prozessen haben wir hohe Qualitätsstandards. Zugleich ergänzen wir die hohe Qualität durch unsere maximale Verlässlichkeit. Dies betrifft nicht nur die elektronischen Bauteile, sondern auch unser Unternehmen selbst. Denn auf uns ist jederzeit absolut Verlass. Daher unterstützen wir Sie mit der effizienten und schnellen Beschaffung abgekündigter Bauelemente sowie der Lieferung direkt zu Ihrem Produktionsstandort. Dadurch schließen wir Engpässe und beliefern Sie mit hochwertigen Bauteilen, die Sie für Ihre Produktion brauchen.
             </p>
+
           </div>
-
-
 
         </div>
 
 
-
       </div>
+
     </div>
 
 
 
-    <div style="margin-left: 0.5%; width: 100.1%;">
-     <?php include "../nav/footer.html" ?>
-     </div>
+
+
+
+
 
 
   </div>
 
+  <footer>
+    <?php include "../nav/footer.html" ?>
+  </footer>
 
+  </div>
 
-
-
-
-  <script src="../static/js/jquery.js" type="text/javascript"></script>
-  <script src="../static/js/bootstrap.js" type="text/javascript"></script>
-  <script src="../static/js/modal.js" type="text/javascript"></script>
-  <script src="../static/js/slider.js" type="text/javascript"></script>
+  <script src="/static/js/main.js" type="text/javascript"></script>
+  <script src="/static/js/slider.js" type="text/javascript"></script>
 </body>
 
 </html>
